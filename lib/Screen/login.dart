fbd1dd47e6b0e1a8a08056de3b33e30032919680
cosmos0520 <cosmos20040520@gmail.com>
2021-10-08 09:51:30 +0900

    making loginPage

---
+++ lib/Screen/login.dart
@@ -0,0 +1,69 @@
+// import 'package:flutter/material.dart';
+
+// class Login extends StatefulWidget {
+//   @override
+//   _LoginState createState() => _LoginState();
+// }
+
+// class _LoginState extends State<Login> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SafeArea(
+//         child: ListView(
+//           padding: EdgeInsets.symmetric(horizontal: 24),
+//           children: <Widget>[
+//             SizedBox(
+//               height: 95,
+//             ),
+//             Row(
+//               children: <Widget>[
+//                 SizedBox(
+//                   child: Image.asset('../image/Logo.png'),
+//                   height: 100,
+//                   width: 100,
+//                 ),
+//                 SizedBox(
+//                   height: 50,
+//                 ),
+//                 Text(
+//                   '누리',
+//                   style: TextStyle(fontSize: 24),
+//                 ),
+//                 SizedBox(
+//                   height: 100,
+//                 ),
+//                 RaisedButton(
+//                   child: Text('google', style: TextStyle(fontSize: 24)),
+//                   onPressed: () => Text('Google'),
+//                 ),
+//                 RaisedButton(
+//                   child: Text('Naver', style: TextStyle(fontSize: 24)),
+//                   onPressed: () => Text('Naver'),
+//                 ),
+//                 RaisedButton(
+//                   child: Text('KaKao', style: TextStyle(fontSize: 24)),
+//                   onPressed: () => Text('KaKao'),
+//                 ),
+//                 SizedBox(
+//                   height: 7,
+//                 ),
+//                 Text(
+//                   '또는',
+//                   style: TextStyle(fontSize: 12),
+//                 ),
+//                 SizedBox(
+//                   height: 7,
+//                 ),
+//                 RaisedButton(
+//                   child: Text('계정 만들기', style: TextStyle(fontSize: 24)),
+//                   onPressed: () => Text('계정 만들기'),
+//                 ),
+//               ],
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
